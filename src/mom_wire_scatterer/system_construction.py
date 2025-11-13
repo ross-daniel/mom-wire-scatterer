e8b99eb96434b92f74b569b0a5f30a4208733191
ross-daniel <ross.stauder@yahoo.com>
2025-11-13 16:33:22 -0700

    wrote class to construct Zmn matrix [UNTESTED]

---
+++ src/mom_wire_scatterer/system_construction.py
@@ -0,0 +1,51 @@
+import numpy as np
+import src.mom_wire_scatterer.constants as constants
+import src.mom_wire_scatterer.numerical_integration as numerical_integration
+
+class ZConstructor:
+    def __init__(self, num_divisions: int, wire_length: float, wire_radius: float, omega: float,
+                 approx_degree: int = 6):
+        self.N, self.M = num_divisions, num_divisions
+        self.L = wire_length
+        self.a = wire_radius
+        self.omega = omega
+        self.beta = 2 * np.pi * constants.c_0 / self.omega
+        self.d = self.L / self.N
+        self.approx_degree = approx_degree
+        self.Z = np.zeros((self.N, self.M))
+        for m in range(self.N):
+            for n in range(self.M):
+                self.Z[m, n] = self.calculate_Zmn(m, n)
+
+    def calculate_Zmn(self, m, n):
+        if m == n:
+            magnetic_potential_term = self.psi_1_eq(m, n)
+        else:
+            magnetic_potential_term = self.psi_1_neq(m, n)
+        electric_potential_term = self.psi_2(m, n)
+        return (complex(0, 1) * self.omega * constants.mu_0 *
+                (magnetic_potential_term +
+                 (1 / (self.d * self.beta ** 2)) * electric_potential_term))
+
+    def psi_1_eq(self, m, n):
+        R = lambda x: np.sqrt(x ** 2 + self.a ** 2)
+        int1 = lambda x: (1 - x / self.d) * np.cos(self.beta * R(x)) / R(x) - np.cos(self.beta * self.a) / R(x)
+        int2 = lambda x: (1 - x / self.d) * np.sin(self.beta * R(x)) / R(x)
+        int3 = 2 * np.cos(self.beta * self.a) * np.log((self.d + np.sqrt(self.d ** 2 + self.a ** 2)) / self.a)
+        return (2 * numerical_integration.numerical_integral(int1, self.approx_degree, (0, self.d))
+                - 2j * numerical_integration.numerical_integral(int2, self.approx_degree, (0, self.d))
+                + int3)
+
+    def psi_1_neq(self, m, n):
+        R = lambda x: np.sqrt(((m - n) * self.d - x) ** 2 + self.a ** 2)
+        psi_1 = lambda x: (1 - abs(x) / self.d) * np.exp(-1j * self.beta * R(x)) / R(x)
+        return numerical_integration.numerical_integral(psi_1, self.approx_degree, (-self.d, self.d))
+
+    def psi_2(self, m, n):
+        R_0 = np.sqrt(((n - m)*self.d) ** 2 + self.a ** 2)
+        R_1 = np.sqrt(((n - m - 1)*self.d) ** 2 + self.a ** 2)
+        R_2 = np.sqrt(((n - m + 1)*self.d) ** 2 + self.a ** 2)
+        term1 = np.exp(-1j * self.beta * R_0) / R_0
+        term2 = np.exp(-1j * self.beta * R_1) / R_1
+        term3 = np.exp(-1j * self.beta * R_2) / R_2
+        return term1 - 2 * term2 + term3
